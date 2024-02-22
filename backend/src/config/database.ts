d1b41ded5648fd591448e42702707f4a20e6a056
Eliaquim dos Santos <eliaquimdossantos@gmail.com>
2024-02-21 22:53:02 -0300

    Ajuste nas configurações de BD para Postgres

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -2,16 +2,19 @@ require("../bootstrap");
 
 module.exports = {
   define: {
-    charset: "utf8mb4",
-    collate: "utf8mb4_bin"
+    charset: "utf8",
+    collate: "utf8_general_ci"
   },
-  dialect: process.env.DB_DIALECT || "mysql",
-  timezone: process.env.DB_TIMEZONE || "-03:00",
+  dialect: process.env.DB_DIALECT || "postgres",
+  timezone: process.env.DB_TIMEZONE || "America/Sao_Paulo",
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   logging: false,
-  seederStorage: "json",
-  seederStoragePath: "sequelizeData.json"
+  seederStorage: "sequelize",
+  seederStorageTableName: "sequelizeData",
+  pool: {
+    max: 2,  // quantidade máxima de conexões no pool
+  }
 };
